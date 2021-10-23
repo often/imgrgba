c1edef89f5f38cfd030d37486f04e03c27b59092
Sany <44934721+often@users.noreply.github.com>
2021-10-23 12:15:20 -0500

    add lib

---
+++ lib.js
@@ -0,0 +1,9 @@
+const getRgba = img =>
+{
+	const ctx = document.createElement('canvas').getContext('2d')
+	ctx.drawImage(img, 0, 0, 1, 1)
+	
+	return Array.from(ctx.getImageData(0, 0, 1, 1).data)
+}
+
+HTMLImageElement.prototype.getRgba = function() { return getRgba(this) }
